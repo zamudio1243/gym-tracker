dafe3ba85d159ee97cd02b9bdcdfbd5eed55f50a
Héctor Zamudio <47469444+zamudio1243@users.noreply.github.com>
2026-02-25 23:40:24 -0600

    🚧 sign-up page

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,11 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./prisma";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  plugins: [nextCookies()],
 });
